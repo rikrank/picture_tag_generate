0a63f30d2ffd2fabcbca7af37276d447ece7e0a3
rikrank <morishita-r@tam-tam.co.jp>
2022-06-14 22:30:57 +0900

    Feat：pictureタグ生成スクリプト作成

--- index.js
+++ index.js
@@ -1,92 +1,70 @@
 const fs = require('fs')
 const dir = `./src/img`;
-const files = fs.readdirSync(dir);
 const sizeOf = require("image-size");
 const glob = require("glob");
 
-const allowExtensions = ".(jpeg|jpg|JPG|png|webp|bmp|gif)$";
-const pattern = "./src/img/**/*";
+const ALLOW_EXTENTION = ".(jpeg|jpg|JPG|png|webp|bmp|gif)$";
+const ENTRY_POINT = "./src/img/**/*";
 
+const genHtml = (img) => {
+    const { jpgValue, webpValue, width, height } = img;
 
-const genHtml = (jpg, webp, src, width, height) => {
     const pictureHtml = `
     <picture>
-        <source srcset="${src}${webp}" type="image/webp" width="${width}" height="${height}" />
-        <img src="${src}${jpg}" alt="" width="${width}" height="${height}" />
+        <source srcset="${webpValue}" type="image/webp" width="${width}" height="${height}" />
+        <img src="${jpgValue}" alt="" width="${width}" height="${height}" />
     </picture>
     `;
-
+    console.log(pictureHtml);
     return pictureHtml;
 }
 
-const genHtml2 = (jpg, webp) => {
-    const pictureHtml = `
-    <picture>
-        <source srcset="${webp}" type="image/webp" width="300" height="300" />
-        <img src="${jpg}" alt="" width="300" height="300" />
-    </picture>
-    `;
-
-    return pictureHtml;
-}
-
+const sliceByNumber = (array, number) => {
+    const length = Math.ceil(array.length / number);
+    return new Array(length).fill().map((_, i) => array.slice(i * number, (i + 1) * number));
+};
 
 const genHTMLElementHandler = () => {
-    glob(pattern, (err, files) => {
+    glob(ENTRY_POINT, (err, files) => {
         if (err) {
             console.log(err); return;
         }
 
-        const sliceByNumber = (array, number) => {
-            const length = Math.ceil(array.length / number);
-            return new Array(length).fill().map((_, i) => array.slice(i * number, (i + 1) * number));
-        };
+        const h = files.map((file) => {
+            let dimentions = sizeOf(file);
+            dimentions.fileName = file;
+            return dimentions;
+        })
 
-        const slicedArr = sliceByNumber(files, 2);
-        slicedArr.map((_, i) => genHtml2(_[0], _[1]));
-        const genPictureTags = slicedArr.map((_, i) => genHtml2(_[0], _[1]));
+        const slicedArrH = sliceByNumber(h, 2);
+        const g = slicedArrH.map((item) => {
 
-        const convertedHTMLTags = genPictureTags.join('');
-        fs.writeFile('index.html', convertedHTMLTags, (err) => {
+            const patternDefaultExtention = ALLOW_EXTENTION.includes(item[0].type)
+            const patternWEBP = item[1].type === 'webp';
+
+            const defaultImgValue = patternDefaultExtention ? item[0].fileName : ''; // 空だったら、fileNameは""
+            const webpImgValue = patternWEBP ? item[1].fileName : ''; // 空だったら、fileNameは""
+            const width = item[0].width;
+            const height = item[0].height;
+
+            if (patternDefaultExtention && patternWEBP) {
+                return {
+                    jpgValue: defaultImgValue, webpValue: webpImgValue, width, height
+                };
+            }
+        })
+        const html = g.map((item) => {
+            return genHtml(item);
+        })
+
+        const convertedHtml = html.join('');
+
+        fs.writeFile('index.html', convertedHtml, function (err) {
             if (err) { throw err; }
             console.log('index.htmlが作成されました');
         });
-
-        // files.forEach(file => {
-        //     genHtml(file)
-        // })
-
-        // const h = files.map((file) => {
-        //     const dimentions = sizeOf(file);
-        //     return dimentions;
-        // })
     });
+
 }
 
-genHTMLElementHandler();
-
-// const genHTMLElementHandler = (src, width, height) => {
-//     const targetFiles = files.filter((file) => {
-//         const extention = file.split(".").pop();
-//         const isMedia = allowExtensions.includes(extention);
-//         if (isMedia) {
-//             return file;
-//         }
-//     });
-
-//     const sliceByNumber = (array, number) => {
-//         const length = Math.ceil(array.length / number);
-//         return new Array(length).fill().map((_, i) => array.slice(i * number, (i + 1) * number));
-//     };
-
-//     const slicedArr = sliceByNumber(targetFiles, 2);
-//     const genPictureTags = slicedArr.map((_, i) => genHtml(_[0], _[1], src, width, height));
-//     const convertedHTMLTags = genPictureTags.join('');
-
-//     fs.writeFile('index.html', convertedHTMLTags, (err) => {
-//         if (err) { throw err; }
-//         console.log('index.htmlが作成されました');
-//     });
-// }
-
-// genHTMLElementHandler("/assets/img/", 300, 300);
+// genHTMLElementHandler();
