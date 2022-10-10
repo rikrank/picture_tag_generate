893f44cdbd1f90d8ebfa9b150d83f38ecf23340a
rikrank <morishita-r@tam-tam.co.jp>
2022-10-10 10:51:56 +0900

    Refactor：変数リファクタリング

--- scripts/picture.js
+++ scripts/picture.js
@@ -25,17 +25,57 @@ const generateSnippets = (img, fileType) => {
     }
 };
 
-const sliceByNumber = (array, number) => {
+const sliceArrByNumber = (array, number) => {
     const length = Math.ceil(array.length / number);
     return new Array(length).fill().map((_, i) => array.slice(i * number, (i + 1) * number));
 };
 
+const createFile = (outputDir, fileType) => {
+    let fileExt;
+    if (fileType === 'html') {
+        fileExt = ".html";
+    } else if (fileType === 'pug') {
+        fileExt = ".pug";
+    }
+    return `./${outputDir}/snippets${fileExt}`;
+}
+
+const inputFileData = (item) => {
+    let imgPath = "";
+    let webpPath = "";
+
+    if (item[0].type === 'webp') {
+        webpPath = item[0].fileName;
+    } else {
+        imgPath = item[0].fileName;
+    }
+
+    const isExistMultipleExt = item[1] || null;
+    if (isExistMultipleExt) {
+        if (item[1].type === 'webp') {
+            webpPath = item[1].fileName;
+        } else {
+            imgPath = "";
+        }
+    }
+
+    const width = item[0].width;
+    const height = item[0].height;
+
+    // コンパイル後の形式にパス変換
+    const replacedImgPath = imgPath.replace('./src/', '/');
+    const replacedWebpPath = webpPath.replace('./src/', '/');
+
+    return {
+        replacedImgPath, replacedWebpPath, width, height
+    };
+}
+
 const generateSnippetsHandler = (fileType) => {
     glob(TARGET_PATTERN, (err, files) => {
-
         // 画像ファイルが存在しなかった場合
         if (!files.length) {
-            console.error('画像ファイルが存在しません');
+            console.error('画像ファイルが存在しません。');
             throw err;
         } else {
             const fileDimentions = files.map((file) => {
@@ -44,62 +84,25 @@ const generateSnippetsHandler = (fileType) => {
                 return dimentions;
             })
 
-            const slicedFileDimentions = sliceByNumber(fileDimentions, 2);
+            const slicedFileDimentions = sliceArrByNumber(fileDimentions, 2);
             const imgValues = slicedFileDimentions.map((item) => {
-
-                let imgPath = "";
-                let webpPath = "";
-
-                if (item[0].type === 'webp') {
-                    webpPath = item[0].fileName;
-                } else {
-                    imgPath = item[0].fileName;
-                }
-
-                const isExistMultipleExt = item[1] || null;
-                if (isExistMultipleExt) {
-                    if (item[1].type === 'webp') {
-                        webpPath = item[1].fileName;
-                    } else {
-                        imgPath = "";
-                    }
-                }
-
-                const width = item[0].width;
-                const height = item[0].height;
-
-                // コンパイル後の形式にパス変換
-                const replacedImgPath = imgPath.replace('./src/', '/');
-                const replacedWebpPath = webpPath.replace('./src/', '/');
-
-                return {
-                    replacedImgPath, replacedWebpPath, width, height
-                };
+                return inputFileData(item);
             })
 
             const resultSource = imgValues.map((item) => {
                 return generateSnippets(item, fileType);
             })
 
-            const outPutDir = (type) => {
+            const createFileDir = (type) => {
                 const OUTPUT_DIR = 'dist';
                 fs.mkdir(OUTPUT_DIR, { recursive: true }, (err) => {
                     if (err) { throw err; }
                 });
-
-                let fileExt;
-
-                if (type === 'html') {
-                    fileExt = ".html";
-                } else if (type === 'pug') {
-                    fileExt = ".pug";
-                }
-
-                return `./${OUTPUT_DIR}/snippets${fileExt}`;
+                return createFile(OUTPUT_DIR, type);
             }
 
             const snippets = resultSource.join('');
-            fs.writeFile(outPutDir(fileType), snippets, function (err) {
+            fs.writeFile(createFileDir(fileType), snippets, (err) => {
                 if (err) {
                     console.error('エラーが発生しました。スニペットを生成できませんでした。');
                     throw err;
@@ -126,4 +129,3 @@ if (inputFileType === 'html' || inputFileType === 'pug') {
 } else {
     console.log("'html' または 'pug' のいずれかを入力してください。");
 }
-
